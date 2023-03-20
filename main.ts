d4888c825c722f5e597b34bf57e74a759f974bfd
Seok Chulho <sukch369@gmail.com>
2023-03-20 22:44:08 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.ts
@@ -0,0 +1,29 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        basic.showIcon(IconNames.Heart)
+        basic.pause(500)
+        basic.clearScreen()
+    }
+    if (receivedNumber == 0) {
+        basic.showIcon(IconNames.No)
+        basic.pause(500)
+        basic.clearScreen()
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(500)
+    basic.clearScreen()
+    radio.sendNumber(1)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.No)
+    basic.pause(500)
+    basic.clearScreen()
+    radio.sendNumber(0)
+})
+radio.setGroup(1)
+radio.setTransmitPower(7)
+basic.forever(function () {
+	
+})
