f48b80e1cf04d16d7f8e08502247518928663882
Seok Chulho <sukch369@gmail.com>
2023-03-20 22:55:50 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,24 +1,24 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
         basic.showIcon(IconNames.Heart)
-        basic.pause(500)
+        basic.pause(200)
         basic.clearScreen()
     }
     if (receivedNumber == 0) {
         basic.showIcon(IconNames.No)
-        basic.pause(500)
+        basic.pause(200)
         basic.clearScreen()
     }
 })
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Heart)
-    basic.pause(500)
+    basic.pause(200)
     basic.clearScreen()
     radio.sendNumber(1)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.No)
-    basic.pause(500)
+    basic.pause(200)
     basic.clearScreen()
     radio.sendNumber(0)
 })
